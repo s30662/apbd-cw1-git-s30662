be03f83f0d3a47dcfcb1d2699ad498351bd152dc
MichMaw <machmichal321@gmail.com>
2026-03-11 12:50:07 +0100

    fixed gitgnore and added dodajWektory method

--- apbd-cw1-git-s30662/Program.cs
+++ apbd-cw1-git-s30662/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+double[] wektor1 = { 5, 10, 4, 2, 3 };
+double[] wektor2 = { 5, 10, 4, 2, 3 };
+Console.WriteLine(dodajWektory(wektor1,wektor2));
+double[] dodajWektory(double[] wektor1, double[] wektor2)
+{
+    int a = wektor1.Length > wektor2.Length ? wektor1.Length : wektor2.Length;
+   double[] result = new double[a];
+   for (int i = 0; i < a; i++)
+   {
+      result[i] = wektor1[i] + wektor2[i];
+   }
+   return result;
+}
