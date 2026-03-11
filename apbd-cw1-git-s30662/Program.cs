c7ca881c17d088b7a0128bd5ab9772ce47045dbc
MichMaw <machmichal321@gmail.com>
2026-03-11 13:03:13 +0100

    Added user interaction

--- apbd-cw1-git-s30662/Program.cs
+++ apbd-cw1-git-s30662/Program.cs
@@ -2,6 +2,37 @@
 double[] wektor1 = { 5, 10, 4, 2, 3 };
 double[] wektor2 = { 5, 10, 4, 2, 3 };
 Console.WriteLine(DodajWektory(wektor1,wektor2));
+Boolean working = true;
+while (working)
+{
+    String userInput = Console.ReadLine();
+    switch (userInput)
+    {
+        case "help":
+            Console.WriteLine("vec {1,2}: create vectors");
+            Console.WriteLine("add: adds vectors");
+            Console.WriteLine("sub: subtracts vectors");
+            break;
+        case "add":
+            if (wektor1.Length <= 0 || wektor2.Length <= 0)
+            {
+                Console.WriteLine(DodajWektory(wektor1, wektor2));
+            }
+
+            break;
+        case "sub":
+            if (wektor1.Length <= 0 || wektor2.Length <= 0)
+            {
+                Console.WriteLine(OdejmijWektory(wektor1, wektor2));
+            }
+
+            break;
+        case "exit":
+            working = false;
+            break;
+    }
+}
+
 double[] DodajWektory(double[] wektor1, double[] wektor2)
 {
     int a = wektor1.Length > wektor2.Length ? wektor1.Length : wektor2.Length;
