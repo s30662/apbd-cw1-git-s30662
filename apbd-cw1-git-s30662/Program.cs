93eca5abee4fce61688401326048f16d58e04e49
MichMaw <machmichal321@gmail.com>
2026-03-11 12:55:24 +0100

    Refactor method names and fixed methods to work for diffrent size vectors

--- apbd-cw1-git-s30662/Program.cs
+++ apbd-cw1-git-s30662/Program.cs
@@ -1,24 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 double[] wektor1 = { 5, 10, 4, 2, 3 };
 double[] wektor2 = { 5, 10, 4, 2, 3 };
-Console.WriteLine(dodajWektory(wektor1,wektor2));
-double[] dodajWektory(double[] wektor1, double[] wektor2)
+Console.WriteLine(DodajWektory(wektor1,wektor2));
+double[] DodajWektory(double[] wektor1, double[] wektor2)
 {
     int a = wektor1.Length > wektor2.Length ? wektor1.Length : wektor2.Length;
    double[] result = new double[a];
    for (int i = 0; i < a; i++)
    {
+       if(wektor1.Length > a && wektor2.Length > a)
       result[i] = wektor1[i] + wektor2[i];
    }
    return result;
 }
 
-double[] odejmijWektory(double[] wektor1, double[] wektor2)
+double[] OdejmijWektory(double[] wektor1, double[] wektor2)
 {
     int a = wektor1.Length > wektor2.Length ? wektor1.Length : wektor2.Length;
     double[] result = new double[a];
     for (int i = 0; i < a; i++)
     {
+        if(wektor1.Length > a && wektor2.Length > a)
         result[i] = wektor1[i] - wektor2[i];
     }
     return result;
